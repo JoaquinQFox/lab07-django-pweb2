0a26374cb6877c845d1264b9b470b69a4e3bdb1f
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-05-31 21:46:40 -0500

    Se definio clase Destinations en models.py y se creo objetos en vista para pasar a index

--- src/travello/views.py
+++ src/travello/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+from .models import Destination
 
 def index(request):
-    return render(request, "index.html")
+    dest1 = Destination()
+    dest1.name = 'Mumbai'
+    dest1.desc = 'The City That Never Sleeps'
+    dest1.price = 700
+    dest1.img = 'destination_1.jpg'
+
+    dest2 = Destination()
+    dest2.name = 'Mexico'
+    dest2.desc = 'Tierra Amarilla'
+    dest2.price = 500
+    dest2.img = 'destination_2.jpg'
+    
+    dest3 = Destination()
+    dest3.name = 'Peru'
+    dest3.desc = 'Rica Cultura'
+    dest3.price = 800
+    dest3.img = 'destination_3.jpg'
+
+    dests = [dest1, dest2, dest3]
+    
+    return render(request, "index.html", {'dests': dests})
