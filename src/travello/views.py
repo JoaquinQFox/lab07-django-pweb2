cc22c51722907c8c087a4212bf1b8e85ef072fc5
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-05-31 23:58:44 -0500

    Se modifica la vista para obtener datos de tabla Destinations y en index se muestra

--- src/travello/views.py
+++ src/travello/views.py
@@ -2,27 +2,6 @@ from django.shortcuts import render
 from .models import Destination
 
 def index(request):
-    dest1 = Destination()
-    dest1.name = 'Mumbai'
-    dest1.desc = 'The City That Never Sleeps'
-    dest1.price = 700
-    dest1.img = 'destination_1.jpg'
-    dest1.offer = False
-
-    dest2 = Destination()
-    dest2.name = 'Mexico'
-    dest2.desc = 'Tierra Amarilla'
-    dest2.price = 500
-    dest2.img = 'destination_2.jpg'
-    dest2.offer = True
-    
-    dest3 = Destination()
-    dest3.name = 'Peru'
-    dest3.desc = 'Rica Cultura'
-    dest3.price = 800
-    dest3.img = 'destination_3.jpg'
-    dest3.offer = True
-
-    dests = [dest1, dest2, dest3]
-    
+    dests = Destination.objects.all()
+        
     return render(request, "index.html", {'dests': dests})
