20ae857a257fe0b77f0efe31a1e897dcba4abd33
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-06-01 19:28:01 -0500

    Se crea vista crear_destino para añadirs destinos a la tabla de destinos

--- src/travello/views.py
+++ src/travello/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Destination
 
 def index(request):
@@ -10,11 +10,30 @@ def lista_destinos(request):
     destinos = Destination.objects.all()
     return render(request, "lista_destinos.html", {'destinos': destinos})
 
+def crear_destino(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        desc = request.POST['desc']
+        price = request.POST['price']
+        offer = 'offer' in request.POST
+        img = request.FILES.get('img')
+
+        destino = Destination(
+            name=name,
+            desc=desc,
+            price=price,
+            offer=offer,
+            img=img
+        )
+
+        destino.save()
+        return redirect("lista_destinos")
+    
+    else:
+        return render(request, 'crear_destino.html')
+
 def editar_destino(request):
     pass
 
 def eliminar_destino(requets):
-    pass
-
-def crear_destino(requets):
     pass
